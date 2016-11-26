71a9b9aae7a45ade263cdb66d6e40dc74836dfb4
Branden Gunn <engunneer@gmail.com>
2016-11-25 23:41:08 -0500

    base Rider project
    blank project!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrinterApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
