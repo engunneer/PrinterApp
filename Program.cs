44ea8628312cd4b612494540358eca9bda2ddd3b
Branden Gunn <engunneer@gmail.com>
2016-11-26 23:38:35 -0500

    can serve basic page

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
+using PrinterApp.Models;
 
 namespace PrinterApp
 {
     internal class Program
     {
+        static SimpleHTTPServer _server;
+
         public static void Main(string[] args)
         {
 
             Console.WriteLine($"Hello, World");
-            ;
+
+            //start a webserver on the default port
+            string serverPath;
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+                serverPath = @"/home/pi/PrinterApp/wwwroot";
+            else
+                serverPath = @"..\..\wwwroot";
+            _server = new SimpleHTTPServer(serverPath, 80);
+
+
         }
     }
 }
